9340783ab4186464150f97fb63e39a099951b374
SantiRomero12 <131960352+SantiRomero12@users.noreply.github.com>
2023-06-16 12:16:59 +0200

    ObjectsList.cpp

--- ObjectsList.cpp
+++ ObjectsList.cpp
@@ -1,2 +1,35 @@
+#include "ObjectsList.h"
+#include "Alien.h"
 
-  // PONER AQUI VUESTRO CODIGO 
+ObjectsList::ObjectsList() {
+    t = 0;
+    theShip = new Ship();
+    L.add(theShip);
+}
+
+ObjectsList::~ObjectsList() {
+    // Se destruyen automáticamente los objetos de la lista al destruir ObjectsList
+    delete theShip;
+}
+
+void ObjectsList::move() {
+    L.move();
+}
+
+void ObjectsList::draw() {
+    L.draw();
+}
+
+void ObjectsList::add(Shape* s) {
+    L.add(s);
+}
+
+void ObjectsList::remove(Shape* s) {
+    L.remove(s);
+}
+
+Ship* ObjectsList::getShip() {
+    return theShip;
+}
+int collisions(Bullet* theBullet, Ship* theShip){
+return 0;}
